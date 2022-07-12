c2ab15628005c7d7934a75b8c5483dc3ee58f4d7
Sandeep Sharma <sandeepisthename007@gmail.com>
2022-07-12 20:10:30 +0530

    Updates

---
+++ precache-manifest.2bf9e89ab3b4fffdabbc25cf298a83a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062e678abe339ca88908fa4337e33735",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3ad38b5d6897b0948509",
+    "url": "/test/static/css/2.23a9c941.chunk.css"
+  },
+  {
+    "revision": "c8ea571064542552ef7b",
+    "url": "/test/static/css/main.a530b0a1.chunk.css"
+  },
+  {
+    "revision": "3ad38b5d6897b0948509",
+    "url": "/test/static/js/2.d6603b24.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test/static/js/2.d6603b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ea571064542552ef7b",
+    "url": "/test/static/js/main.3b020dce.chunk.js"
+  },
+  {
+    "revision": "bd95907802865df93aae",
+    "url": "/test/static/js/runtime-main.def3d777.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
